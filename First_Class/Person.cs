9f2eb7a630651817be9dacbb8cef0bc98e8819c0
sibeldogru <sibeldogru@gmail.com>
2025-06-02 17:04:59 +0300

    Add project files.

---
+++ First_Class/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Class
+{
+    internal class Person
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime DogumGunu { get; set; } 
+    }
+}
---
+++ First_Class/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace First_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ogrenci1 = new Person();
+
+            ogrenci1.Ad = "Sibel";
+            ogrenci1.Soyad = "Doğru";
+            ogrenci1.DogumGunu = new DateTime(1987, 08, 18);
+
+
+            Person ogrenci2 = new Person();
+
+            ogrenci2.Ad = "Ali";
+            ogrenci2.Soyad = "Yılmaz";
+            ogrenci2.DogumGunu = new DateTime(1991, 03, 12);
+
+
+            Person ogretmen = new Person();
+            ogretmen.Ad = "Mehmet";
+            ogretmen.Soyad = "Soytürk";
+            ogretmen.DogumGunu = new DateTime(1973, 01, 15);
+
+            Console.WriteLine("ÖĞRENCİ BİLGİLERİ");
+            Console.WriteLine("Öğrencinin Adı: " + ogrenci1.Ad);
+            Console.WriteLine("Öğrencinin Soyadı: " + ogrenci1.Soyad);
+            Console.WriteLine("Öğrencinin Doğum Tarihi: " + ogrenci1.DogumGunu.ToShortDateString());
+            Console.WriteLine();
+
+
+            Console.WriteLine("ÖĞRENCİ BİLGİLERİ");
+            Console.WriteLine("Öğrencinin Adı: " + ogrenci2.Ad);
+            Console.WriteLine("Öğrencinin Soyadı: " + ogrenci2.Soyad);
+            Console.WriteLine("Öğrencinin Doğum Tarihi: " + ogrenci2.DogumGunu.ToShortDateString());
+            Console.WriteLine();
+
+            Console.WriteLine("ÖĞRETMEN BİLGİLERİ");
+            Console.WriteLine("Öğretmenin Adı: " + ogretmen.Ad);
+            Console.WriteLine("Öğrencinin Soyadı: " + ogretmen.Soyad);
+            Console.WriteLine("Öğrencinin Doğum Tarihi: " + ogretmen.DogumGunu.ToShortDateString());
+
+
+        }
+    }
+}
